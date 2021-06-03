d4643663b65ed72ba7ba3683a2fdcf2c774e52c1
Samu Chakraborty <61682653+samuchakraborty@users.noreply.github.com>
2021-06-03 12:39:17 +0600

    delete by id done

--- app.js
+++ app.js
@@ -24,10 +24,9 @@ app.get("/getAll", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      console.log("connection id : " + connection.threadId);
 
       connection.query("SELECT * from emp1", (err, result) => {
-        connection.release();
+      //  connection.release();
 
         if (!err) {
           res.status(200).json({
@@ -48,12 +47,12 @@ app.get("/getAll", function (req, res) {
 
 //get specified data by id
 
-app.get("/getData/:id", function (req, res) {
+app.get("/getAll/:id", function (req, res) {
   pool.getConnection((err, connection) => {
     if (err) {
       console.log(err);
     } else {
-      console.log("connection id : " + connection.threadId);
+    //  console.log("connection id : " + connection.threadId);
 
       connection.query(
         "SELECT * from emp1 where id=" + req.params.id,
@@ -61,6 +60,8 @@ app.get("/getData/:id", function (req, res) {
           connection.release();
 
           if (!err) {
+
+            
             res.status(200).json({
               status: "Fetch data for id:   " + req.params.id,
               data: result,
@@ -85,12 +86,11 @@ app.post("/insertData", function (req, res) {
       console.log(err);
     } else {
       const name1 = req.body.name;
-      console.log(name1);
+
       const designation1 = req.body.designation;
       const salary1 = req.body.salary;
 
       var sql2 = "SELECT * from emp1";
-      console.log("connection id : " + connection.threadId);
       var sql =
         'INSERT INTO emp1 (name, designation, salary) VALUES ("' +
         name1 +
@@ -99,39 +99,31 @@ app.post("/insertData", function (req, res) {
         '", "' +
         salary1 +
         '")';
-      console.log(sql);
+   
       connection.query(sql, (err, result) => {
-        connection.release();
+     //   connection.release();
 
-          if (!err) {
-              var queryForLastId = "SELECT * from emp1 where id = "+ result.insertId;
-            connection.query(queryForLastId, (err, result) => {
-              //  connection.release();
-        
-                if (!err) {
-                  res.status(200).json({
-                    status: "Successfully added",
-                    data: result,
-                  });
-                } else {
-                  res.status(404).json({
-                    status: "you have no right",
-                    err: err,
-                  });
-                  console.log(err);
-                }
+        if (!err) {
+          var queryForLastId =
+            "SELECT * from emp1 where id = " + result.insertId;
+          connection.query(queryForLastId, (err, result) => {
+            //  connection.release();
+
+            if (!err) {
+              res.status(200).json({
+                status: "Successfully added",
+                data: result,
               });
+            } else {
+              res.status(404).json({
+                status: "you have no right",
+                err: err,
+              });
+              console.log(err);
+            }
+          });
 
-
-
-        //   res.json({
-        //     message: "sucess",
-        //     data: result.insertId,
-        //   });
-          // res.status(200).json({
-          //   status: "Fetch data for id:   " + req.params.id,
-          //   data: result,
-          // });
+    
         } else {
           res.status(404).json({
             status: "you have no right",
@@ -143,6 +135,39 @@ app.post("/insertData", function (req, res) {
     }
   });
 });
+
+app.get('/delete/:id', function (req, res) {
+  
+  pool.getConnection((err, connection) => {
+    if(err) {
+      console.log(err);
+    
+    } else{
+      var sql = 'DELETE FROM emp1 WHERE id=' + req.params.id;
+      console.log(sql);
+      connection.query(sql, (err, result) => {
+        
+        if (!err) {
+          
+          res.json({ success: 1 , msg: "Successfully deleted id: "+ req.params.id});
+        }
+      });
+
+    }
+    
+  
+
+
+
+
+  });
+
+
+
+});
+
+
+
 
 app.listen(port, () => {
   console.log("port is start : " + port);
